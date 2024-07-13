78ccb015e2ca364e3338ae6f4477c7d72aaf75fa
Tw1xie <156274805+Tw1xie@users.noreply.github.com>
2024-07-14 01:40:33 +0200

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -1,38 +1,41 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   const sentences = [
-    " Welcome to my website!",
-    " I'm a young Web Developer.",
-    " Owner of ApathyMC.",
-    " Stay tuned for more updates."
+      "<span class='normal'> Welcome to my Website!</span>",
+      "<span class='normal'> I'm a young <span class='highlight'>Web Developer.</span></span>",
+      "<span class='normal'> Owner of <span class='highlight'>ApathyMC.</span></span>",
+      "<span class='normal'> Stay tuned for more updates.</span>"
   ];
 
   const typingElement = document.getElementById('typer-text');
   let sentenceIndex = 0;
 
   function type() {
-    const currentSentence = sentences[sentenceIndex];
-    let index = 0
-    let typing = setInterval(() => {
-      typingElement.textContent += currentSentence[index];
-      index++;
-      if (index >= currentSentence.length) {
-        clearInterval(typing);
-        setTimeout(deleteText, 1000);
-      }
-    }, 100);
+      typingElement.innerHTML = ""; // Clear previous content
+      const currentSentence = sentences[sentenceIndex];
+      let index = 0;
+      let typing = setInterval(() => {
+          typingElement.innerHTML += currentSentence[index];
+          index++;
+          if (index >= currentSentence.length) {
+              clearInterval(typing);
+              setTimeout(deleteText, 1000);
+          }
+      }, 100);
 
-    function deleteText() {
-      typing = setInterval(() => {
-        typingElement.textContent = currentSentence.substring(0, index);
-        index--;
-        if (index === 0) {
-          clearInterval(typing);
-          sentenceIndex = (sentenceIndex + 1) % sentences.length;
-          setTimeout(type, 1000);
-        }
-      }, 50);
-    }
+      function deleteText() {
+          let indexToDelete = currentSentence.length;
+          typing = setInterval(() => {
+              indexToDelete--;
+              if (indexToDelete >= 0) {
+                  typingElement.innerHTML = currentSentence.substring(0, indexToDelete);
+              } else {
+                  clearInterval(typing);
+                  sentenceIndex = (sentenceIndex + 1) % sentences.length;
+                  setTimeout(type, 1000);
+              }
+          }, 50);
+      }
   }
 
   type();
-})
+});
