b9fa35e79efb3309f9e7054355a78a2e4d693707
Tw1xie <156274805+Tw1xie@users.noreply.github.com>
2024-07-14 00:57:16 +0200

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -1,26 +1,38 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const text = "I'm a young Web Developer";
-    const typingElement = document.getElementById('typer-text');
-    let index = 0;
-    let deleting = false;
-  
-    function type() {
-      if (!deleting && index < text.length) {
-        typingElement.textContent += text[index];
-        index++;
-        setTimeout(type, 200);
-      } else if (deleting && index > 0) {
-        typingElement.textContent = typingElement.textContent.slice(0, -1);
-        index--;
-        setTimeout(type, 100);
-      } else if (index === text.length) {
-        deleting = true;
-        setTimeout(type, 1000);
-      } else if (index === 0) {
-        deleting = false;
-        setTimeout(type, 500);
+  const sentences = [
+    " Welcome to my website!",
+    " I'm a young Web Developer.",
+    " Owner of ApathyMC.",
+    " Stay tuned for more updates."
+  ];
+
+  const typingElement = document.getElementById('typer-text');
+  let sentenceIndex = 0;
+
+  function type() {
+    const currentSentence = sentences[sentenceIndex];
+    let index = 0
+    let typing = setInterval(() => {
+      typingElement.textContent += currentSentence[index];
+      index++;
+      if (index >= currentSentence.length) {
+        clearInterval(typing);
+        setTimeout(deleteText, 1000);
       }
+    }, 100);
+
+    function deleteText() {
+      typing = setInterval(() => {
+        typingElement.textContent = currentSentence.substring(0, index);
+        index--;
+        if (index === 0) {
+          clearInterval(typing);
+          sentenceIndex = (sentenceIndex + 1) % sentences.length;
+          setTimeout(type, 1000);
+        }
+      }, 50);
     }
-  
-    type();
-  });
+  }
+
+  type();
+})
